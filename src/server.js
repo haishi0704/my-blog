e624e2bb1eae95d613326fe61c26779b1931193d
郑苔施 <zhengtaishi@192-168-1-113.tpgi.com.au>
2021-07-05 19:46:23 +1000

    First commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import express from "express"
-import bodyParser from "body-parser"
-import {MongoClient} from "mongodb"
-import path from 'path'
+import express from "express";
+import bodyParser from "body-parser";
+import {MongoClient} from "mongodb";
+import path from 'path';
 
 const app = express();
 app.use(bodyParser.json());
